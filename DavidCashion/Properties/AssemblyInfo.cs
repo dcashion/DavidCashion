f43e53d7868fc6bae6b2bdf29a09a66aed39476e
David Cashion <dcashion@outlook.com>
2016-10-10 19:04:32 -0600

    Updated Nav Bar and Copyright

--- DavidCashion/Properties/AssemblyInfo.cs
+++ DavidCashion/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DavidCashion")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("David's and Zeppelin's Personal Website Application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DavidCashion")]
